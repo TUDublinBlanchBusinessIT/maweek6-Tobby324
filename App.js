a3f9b277c4db82b74babda450509d0ed0c5e9003
B00165442 Tobby Akinwale <B00165442@mytudublin.ie>
2025-10-20 16:51:11 +0100

    completed part 3

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Image, Dimensions } from 'react-native';
+import { StyleSheet, View, Image, Dimensions, Text, TouchableOpacity, Alert } from 'react-native';
+import { useState } from 'react';
 import Swiper from 'react-native-swiper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Crypto from 'expo-crypto';
 import PersonalInfo from './components/PersonalInfo';
 import MovieBooking from './components/MovieBooking';
 
 export default function App() {
+  var uuid = Crypto.randomUUID();
+
   const [booking, setBooking] = useState({
     bookDate: "2000-02-02",
     movieTitle: "",
@@ -12,37 +16,69 @@ export default function App() {
     balcony: 0,
   });
 
+  async function saveData() {
+    const uuid = Crypto.randomUUID();
+    await AsyncStorage.setItem(uuid, JSON.stringify(booking));
+    alert("Saved with UUID: " + uuid);
+    Alert.alert("Saved with UUID: " + uuid);
+  }
+
+      async function getData() {
+        try {
+            let thisBooking = await AsyncStorage.getItem(uuid);
+            let parsedBooking = JSON.parse(thisBooking); // ✅ Convert from string to object
+            Alert.alert("Movie Title: " + parsedBooking.movieTitle);
+        } catch (error) {
+          Alert.alert("Error getting data: " + error.message);
+        }
+      }
+
   return (
     <View style={styles.screencontainer}>
       <View style={styles.imgview}>
         <Image source={require('./assets/moviesV3.png')} />
       </View>
+
       <Swiper showsButtons={true}>
         <PersonalInfo screenstyle={styles.screen} data={booking} setData={setBooking} />
         <MovieBooking screenstyle={styles.screen} data={booking} setData={setBooking} />
       </Swiper>
+
+      <TouchableOpacity style={styles.button} onPress={saveData}>
+        <Text style={{ fontSize: 24, fontWeight: "bold" }}>Save Data</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={getData}>
+        <Text style={{ fontSize: 24, fontWeight: "bold" }}>Get Data</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 var width = Dimensions.get('window');
+
 const styles = StyleSheet.create({
   imgview: {
     flexDirection: "row",
     justifyContent: "center",
     flex: 0.5,
-    marginTop: "8%"
+    marginTop: "8%",
   },
   screencontainer: {
     flexDirection: "column",
     flex: 1,
     padding: "1%",
-    backgroundColor: "lightgrey"
+    backgroundColor: "lightgrey",
   },
   screen: {
     flex: 1,
     alignItems: "start",
-    padding: "10%"
-  }
+    padding: "10%",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "lightblue",
+    padding: 12,
+    margin: 20,
+    borderRadius: 10,
+  },
 });
-
